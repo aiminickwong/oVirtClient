3d2fa30ca8355ffc5393f2df84d49059170d6093
everyun <everyun2012@gmail.com>
2015-04-13 08:48:01 +0800

    登陆接口完成

--- oVirtClinet/Form1.Designer.cs
+++ oVirtClinet/Form1.Designer.cs
@@ -34,11 +34,13 @@
             this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(119, 36);
+            this.textBox1.Location = new System.Drawing.Point(120, 25);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(172, 23);
@@ -47,7 +49,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 39);
+            this.label1.Location = new System.Drawing.Point(34, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 17);
             this.label1.TabIndex = 1;
@@ -56,7 +58,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(57, 90);
+            this.label2.Location = new System.Drawing.Point(58, 67);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 17);
             this.label2.TabIndex = 2;
@@ -65,7 +67,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(69, 142);
+            this.label3.Location = new System.Drawing.Point(70, 110);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(32, 17);
             this.label3.TabIndex = 3;
@@ -73,23 +75,45 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(119, 90);
+            this.textBox2.Location = new System.Drawing.Point(120, 67);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(172, 23);
             this.textBox2.TabIndex = 4;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(119, 142);
+            this.textBox3.Location = new System.Drawing.Point(120, 110);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(172, 23);
             this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "登陆";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(203, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(331, 208);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
@@ -113,6 +137,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- oVirtClinet/Form1.cs
+++ oVirtClinet/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
 
 namespace oVirtClinet
 {
@@ -15,6 +17,52 @@ namespace oVirtClinet
         {
             InitializeComponent();
             Text = "欢迎使用 OvirtClient";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string serverUrl = textBox1.Text.Trim();
+            string username = textBox2.Text.Trim();
+            string password = textBox3.Text;
+            serverUrl = "ovirt-engine.studio.hrsoft.net";
+            username = "admin@internal";
+            password = "ta992080fe";
+
+            if(serverUrl == "" && username == "" && password == "")
+            {
+                MessageBox.Show("服务器地址&用户名&密码不能为空","错误");
+            }else
+            {
+                string certUrl = "http://" + serverUrl + "/ca.crt";
+                serverUrl = "https://" + serverUrl + "/api";
+                string tmpPath = System.IO.Path.GetTempPath()+ "ca.crt";
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(certUrl, tmpPath);
+                HttpWebRequest request = WebRequest.Create(serverUrl) as HttpWebRequest;
+                request.Credentials = new NetworkCredential(username, password);
+
+                request.ClientCertificates.Add(System.Security.Cryptography.X509Certificates.X509Certificate.CreateFromCertFile(tmpPath));
+               
+                try
+                {
+                    System.Net.ServicePointManager.ServerCertificateValidationCallback = (senderX, certificate, chain, sslPolicyErrors) => { return true; };
+                    HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                    if(request.HaveResponse == true && response != null)
+                    {
+                        StreamReader reader = new StreamReader(response.GetResponseStream());
+                        StringBuilder sbSource = new StringBuilder(reader.ReadToEnd());
+                        MessageBox.Show(sbSource.ToString());
+                    }
+                }catch (WebException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
         }
     }
 }
